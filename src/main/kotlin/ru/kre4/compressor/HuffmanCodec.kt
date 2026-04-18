5e739bc3ab2f79476a29bb87680bc1604c09ea33
Kre4 <denhol2002@gmail.com>
2026-04-18 20:07:15 +0300

    [codec] move some classes to separate file

--- src/main/kotlin/ru/kre4/compressor/HuffmanCodec.kt
+++ src/main/kotlin/ru/kre4/compressor/HuffmanCodec.kt
@@ -1,8 +1,7 @@
 package ru.kre4.compressor
 
 import ru.kre4.compressor.extensions.toUnsignedByte
-import java.io.ByteArrayOutputStream
-import java.util.PriorityQueue
+import java.util.*
 
 class HuffmanCodec {
 
@@ -120,47 +119,5 @@ class HuffmanCodec {
 
         node.left?.let { buildCodes(it, "$prefix" + "0", table) }
         node.right?.let { buildCodes(it, "$prefix" + "1", table) }
-    }
-
-    private class BitWriter {
-        private val out = ByteArrayOutputStream()
-        private var currentByte = 0
-        private var bitCount = 0
-
-        fun writeBits(bits: String) {
-            for (ch in bits) {
-                val bit = if (ch == '1') 1 else 0
-                currentByte = (currentByte shl 1) or bit
-                bitCount++
-                if (bitCount == 8) {
-                    out.write(currentByte)
-                    currentByte = 0
-                    bitCount = 0
-                }
-            }
-        }
-
-        fun toByteArray(): ByteArray {
-            if (bitCount > 0) {
-                currentByte = currentByte shl (8 - bitCount)
-                out.write(currentByte)
-                bitCount = 0
-                currentByte = 0
-            }
-            return out.toByteArray()
-        }
-    }
-
-    private class BitReader(private val bytes: ByteArray, private val bitLength: Int) {
-        private var pos = 0
-
-        fun readBit(): Int {
-            require(pos < bitLength) { "Attempt to read beyond Huffman bit stream." }
-            val byteIndex = pos / 8
-            val bitIndex = 7 - (pos % 8)
-            val bit = (bytes[byteIndex].toInt() ushr bitIndex) and 1
-            pos++
-            return bit
-        }
     }
 }
---
+++ src/main/kotlin/ru/kre4/compressor/Utils.kt
@@ -0,0 +1,45 @@
+package ru.kre4.compressor
+
+import java.io.ByteArrayOutputStream
+
+class BitWriter {
+    private val out = ByteArrayOutputStream()
+    private var currentByte = 0
+    private var bitCount = 0
+
+    fun writeBits(bits: String) {
+        for (ch in bits) {
+            val bit = if (ch == '1') 1 else 0
+            currentByte = (currentByte shl 1) or bit
+            bitCount++
+            if (bitCount == 8) {
+                out.write(currentByte)
+                currentByte = 0
+                bitCount = 0
+            }
+        }
+    }
+
+    fun toByteArray(): ByteArray {
+        if (bitCount > 0) {
+            currentByte = currentByte shl (8 - bitCount)
+            out.write(currentByte)
+            bitCount = 0
+            currentByte = 0
+        }
+        return out.toByteArray()
+    }
+}
+
+class BitReader(private val bytes: ByteArray, private val bitLength: Int) {
+    private var pos = 0
+
+    fun readBit(): Int {
+        require(pos < bitLength) { "Attempt to read beyond Huffman bit stream." }
+        val byteIndex = pos / 8
+        val bitIndex = 7 - (pos % 8)
+        val bit = (bytes[byteIndex].toInt() ushr bitIndex) and 1
+        pos++
+        return bit
+    }
+}
