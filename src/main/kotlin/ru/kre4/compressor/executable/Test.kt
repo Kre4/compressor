952f472078dc9a465c21141dab62b5604fe25251
Kre4 <denhol2002@gmail.com>
2026-04-18 20:08:12 +0300

    [codec] report + tests

--- src/main/kotlin/ru/kre4/compressor/executable/Test.kt
+++ src/main/kotlin/ru/kre4/compressor/executable/Test.kt
@@ -1,10 +1,20 @@
 package ru.kre4.compressor.executable
 
+import ru.kre4.compressor.BurrowsWheelerTransform
+
 @OptIn(ExperimentalStdlibApi::class)
 fun main() {
     val byte = -128;
     println(byte.toHexString()) // ffffff80
     println(0xFF) // 255
     println(byte and 0xFF) // 128
+    println("---")
 
+    val strToEncode = "bcvd321@creep"
+    val bwt = BurrowsWheelerTransform()
+
+    val encodeBwt = bwt.encode(strToEncode.toByteArray());
+    println(encodeBwt.transformed.decodeToString())
+    val decodedBwt = bwt.decode(encodeBwt.primaryIndex, encodeBwt.transformed)
+    println(decodedBwt.decodeToString())
 }
